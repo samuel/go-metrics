47bc1795beef755c36ad3b5c648e89970989b776
Samuel Stauffer <samuel@descolada.com>
2012-11-19 17:35:52 -0800

    Make Counter an interface
    Rename existing Counter implementation to atomicCounter

--- metrics/counter.go
+++ metrics/counter.go
@@ -5,29 +5,36 @@ import (
 	"sync/atomic"
 )
 
-type Counter int64
+type Counter interface {
+	Inc(delta int64)
+	Dec(delta int64)
+	Set(delta int64)
+	Count() int64
+}
 
-func NewCounter() *Counter {
-	c := Counter(int64(0))
+type atomicCounter int64
+
+func NewCounter() Counter {
+	c := atomicCounter(int64(0))
 	return &c
 }
 
-func (c *Counter) Inc(delta int64) {
+func (c *atomicCounter) Inc(delta int64) {
 	atomic.AddInt64((*int64)(c), delta)
 }
 
-func (c *Counter) Dec(delta int64) {
+func (c *atomicCounter) Dec(delta int64) {
 	atomic.AddInt64((*int64)(c), -delta)
 }
 
-func (c *Counter) Set(value int64) {
+func (c *atomicCounter) Set(value int64) {
 	atomic.StoreInt64((*int64)(c), value)
 }
 
-func (c *Counter) Count() int64 {
+func (c *atomicCounter) Count() int64 {
 	return atomic.LoadInt64((*int64)(c))
 }
 
-func (c *Counter) String() string {
+func (c *atomicCounter) String() string {
 	return strconv.FormatInt(c.Count(), 10)
 }
