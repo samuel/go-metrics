9e1aec93024dfa6cec38b887f5bb999ed40038ff
Samuel Stauffer <samuel@descolada.com>
2012-11-21 21:07:21 -0800

    Rename SystemMetrics to RuntimeMetrics

---
+++ metrics/runtime.go
@@ -0,0 +1,20 @@
+package metrics
+
+import "runtime"
+
+var RuntimeMetrics = &runtimeMetrics{}
+
+type runtimeMetrics struct {
+	memStats runtime.MemStats
+}
+
+func (s *runtimeMetrics) Metrics() map[string]interface{} {
+	runtime.ReadMemStats(&s.memStats)
+	return map[string]interface{}{
+		"Mallocs":         CounterValue(s.memStats.Mallocs),
+		"Frees":           CounterValue(s.memStats.Frees),
+		"heap/HeapAlloc":  GaugeValue(s.memStats.HeapAlloc),
+		"gc/NumGC":        CounterValue(s.memStats.NumGC),
+		"gc/PauseTotalNs": CounterValue(s.memStats.PauseTotalNs),
+	}
+}
