4ff198383c432e41a3293e74550829aee1b37b2d
Samuel Stauffer <samuel@descolada.com>
2012-11-24 00:50:28 -0800

    Add HeapObjects to runtime stats

--- metrics/runtime.go
+++ metrics/runtime.go
@@ -11,10 +11,11 @@ type runtimeMetrics struct {
 func (s *runtimeMetrics) Metrics() map[string]interface{} {
 	runtime.ReadMemStats(&s.memStats)
 	return map[string]interface{}{
-		"Mallocs":         CounterValue(s.memStats.Mallocs),
-		"Frees":           CounterValue(s.memStats.Frees),
-		"heap/HeapAlloc":  GaugeValue(s.memStats.HeapAlloc),
-		"gc/NumGC":        CounterValue(s.memStats.NumGC),
-		"gc/PauseTotalNs": CounterValue(s.memStats.PauseTotalNs),
+		"Mallocs":          CounterValue(s.memStats.Mallocs),
+		"Frees":            CounterValue(s.memStats.Frees),
+		"heap/HeapAlloc":   GaugeValue(s.memStats.HeapAlloc),
+		"heap/HeapObjects": GaugeValue(s.memStats.HeapObjects),
+		"gc/NumGC":         CounterValue(s.memStats.NumGC),
+		"gc/PauseTotalNs":  CounterValue(s.memStats.PauseTotalNs),
 	}
 }
