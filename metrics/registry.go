af83de7cf7f5ba17e171b15ec3ffa976a000f3b3
Samuel Stauffer <samuel@descolada.com>
2012-11-23 16:11:53 -0800

    Add a filtered registry

--- metrics/registry.go
+++ metrics/registry.go
@@ -1,13 +1,27 @@
 package metrics
 
 import (
+	"regexp"
 	"sync"
 )
 
-type Registry struct {
+type Registry interface {
+	Scope(scope string) Registry
+	Add(name string, metric interface{})
+	Remove(name string)
+	Do(f Doer) error
+}
+
+type registry struct {
 	scope   string
 	metrics map[string]interface{}
 	mutex   sync.RWMutex
+}
+
+type filteredRegistry struct {
+	registry Registry
+	include  []*regexp.Regexp
+	exclude  []*regexp.Regexp
 }
 
 type Collection interface {
@@ -16,42 +30,83 @@ type Collection interface {
 
 type Doer func(name string, metric interface{}) error
 
-func NewRegistry() *Registry {
-	return &Registry{
+// Registry
+
+func NewRegistry() Registry {
+	return &registry{
 		metrics: make(map[string]interface{}),
 	}
 }
 
-func (r *Registry) scopedName(name string) string {
+func (r *registry) scopedName(name string) string {
 	if r.scope != "" {
 		return r.scope + "/" + name
 	}
 	return name
 }
 
-func (r *Registry) Scope(scope string) *Registry {
-	return &Registry{
+func (r *registry) Scope(scope string) Registry {
+	return &registry{
 		scope:   r.scopedName(scope),
 		metrics: r.metrics,
 	}
 }
 
-func (r *Registry) Add(name string, metric interface{}) {
+func (r *registry) Add(name string, metric interface{}) {
 	r.mutex.Lock()
 	r.metrics[r.scopedName(name)] = metric
 	r.mutex.Unlock()
 }
 
-func (r *Registry) Remove(name string) {
+func (r *registry) Remove(name string) {
 	r.mutex.Lock()
 	delete(r.metrics, r.scopedName(name))
 	r.mutex.Unlock()
 }
 
-func (r *Registry) Do(f Doer) error {
+func (r *registry) Do(f Doer) error {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 	return do("", r.metrics, f)
+}
+
+// FilteredRegistry
+
+func NewFilterdRegistry(registry Registry, include []*regexp.Regexp, exclude []*regexp.Regexp) Registry {
+	return &filteredRegistry{registry, include, exclude}
+}
+
+func (r *filteredRegistry) Do(f Doer) error {
+	return r.registry.Do(func(name string, metric interface{}) error {
+		if r.exclude != nil {
+			for _, re := range r.exclude {
+				if re.MatchString(name) {
+					return nil
+				}
+			}
+		}
+		if r.include == nil {
+			return f(name, metric)
+		}
+		for _, re := range r.include {
+			if re.MatchString(name) {
+				return f(name, metric)
+			}
+		}
+		return nil
+	})
+}
+
+func (r *filteredRegistry) Scope(scope string) Registry {
+	return &filteredRegistry{r.registry.Scope(scope), r.include, r.exclude}
+}
+
+func (r *filteredRegistry) Add(name string, metric interface{}) {
+	r.registry.Add(name, metric)
+}
+
+func (r *filteredRegistry) Remove(name string) {
+	r.registry.Remove(name)
 }
 
 func do(scope string, metrics map[string]interface{}, f Doer) error {
--- metrics/registry_test.go
+++ metrics/registry_test.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"fmt"
 	"reflect"
+	"regexp"
 	"testing"
 )
 
@@ -19,5 +20,47 @@ func TestRegistry(t *testing.T) {
 	exp := map[string]string{"num": "1", "test/foo": "bar"}
 	if !reflect.DeepEqual(metrics, exp) {
 		t.Fatalf("registry.Do should have returned %+v instead of %+v", exp, metrics)
+	}
+}
+
+func TestFilteredRegistry(t *testing.T) {
+	r := NewRegistry()
+	r.Add("num", 1)
+	r.Add("string", "x")
+
+	// nil include and exclude should make the filter a no-op
+	fr := NewFilterdRegistry(r, nil, nil)
+	out := make(map[string]string)
+	fr.Do(func(name string, metric interface{}) error {
+		out[name] = fmt.Sprintf("%+v", metric)
+		return nil
+	})
+	exp := map[string]string{"num": "1", "string": "x"}
+	if !reflect.DeepEqual(out, exp) {
+		t.Fatalf("filteredRegistry.Do should have returned %+v instead of %+v", exp, out)
+	}
+
+	// includes
+	fr = NewFilterdRegistry(r, []*regexp.Regexp{regexp.MustCompile("^num$")}, nil)
+	out = make(map[string]string)
+	fr.Do(func(name string, metric interface{}) error {
+		out[name] = fmt.Sprintf("%+v", metric)
+		return nil
+	})
+	exp = map[string]string{"num": "1"}
+	if !reflect.DeepEqual(out, exp) {
+		t.Fatalf("filteredRegistry.Do should have returned %+v instead of %+v", exp, out)
+	}
+
+	// excludes
+	fr = NewFilterdRegistry(r, nil, []*regexp.Regexp{regexp.MustCompile("^num$")})
+	out = make(map[string]string)
+	fr.Do(func(name string, metric interface{}) error {
+		out[name] = fmt.Sprintf("%+v", metric)
+		return nil
+	})
+	exp = map[string]string{"string": "x"}
+	if !reflect.DeepEqual(out, exp) {
+		t.Fatalf("filteredRegistry.Do should have returned %+v instead of %+v", exp, out)
 	}
 }
